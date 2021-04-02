c367a412ab3222b40bdceaa180671345ff786534
Liliane Soares <lilianesoares.code@gmail.com>
2021-04-02 14:48:09 -0300

    adding email validation

---
+++ src/components/LoginInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginInput extends React.Component{
+validateEmail = (email) => {
+const isValid = email.match(/^([\w:/.+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+if(!isValid || email.length === 0){
+}
+
+}
+
+
+
+}
