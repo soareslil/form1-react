8df1704ea2da4b563a6d6ba0be2597240b38ace7
Liliane Soares <lilianesoares.code@gmail.com>
2021-04-02 14:22:34 -0300

    creating state, constructor, functions to handle users change

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -4,3 +4,17 @@ import LoginInput from './LoginIunput';
 class LoginForm extends React.Component {
 constructor(){
 super();
+
+this.state ={
+    login: '',
+    password: '',
+    };
+    handleChange = (event) =>{
+    this.setState({
+    [event.target.name]: event.target.value,
+    });
+    }
+    handleSubmit = (event) =>{
+    event.preventDefault();
+    alert('login funcionou');
+    }   
